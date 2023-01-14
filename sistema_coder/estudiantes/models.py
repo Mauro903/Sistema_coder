cc3c0738bec034c0a0e7b5db35f4ed94e5e6b889
Mauro Pisani Curlo <mauro903_7@hotmail.com>
2023-01-14 02:44:33 -0300

    Agregamos objetos

--- sistema_coder/estudiantes/models.py
+++ sistema_coder/estudiantes/models.py
@@ -5,25 +5,37 @@ class Curso(models.Model):
     nombre = models.CharField(max_length=64)
     comision = models.IntegerField() #Numerico
 
+    def __str__(self):
+        return f"{self.nombre}, {self.comision}"
+
 
 class Estudiantes(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     dni = models.CharField(max_length=32)
     email = models.EmailField()
-    fecha_nacimiento = models.DateField()
+    fecha_nacimiento = models.DateField(null=True) #El null=True es para no tener que completarlo
+    
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}" #Para que en el queryset, me diga directamente los strings de los objetos
 
 class Profesor(models.Model):
     nombre = models.CharField(max_length=30) #Caracteres
     apellido = models.CharField(max_length=30)
     dni = models.CharField(max_length=32)
     email = models.EmailField()
-    fecha_nacimiento = models.DateField()
+    fecha_nacimiento = models.DateField(null=True)
     profesion = models.CharField(max_length=128)
-    bio = models.TextField() #Archivo de texto que no tiene limites
+    bio = models.TextField(null=True) #Archivo de texto que no tiene limites
+
+    def __str__(self):
+        return f"{self.apellido}, {self.nombre}"
 
 class Entregable(models.Model):
     nombre = models.CharField(max_length=256)
     fecha_de_entrega = models.DateTimeField()
     is_approved = models.BooleanField(default=False)
     is_desapproved = models.BooleanField(default=False)
+
+class Instituto(models.Model):
+    nombre = models.CharField(max_length=256)
--- sistema_coder/estudiantes/urls.py
+++ sistema_coder/estudiantes/urls.py
@@ -5,6 +5,6 @@ from estudiantes.views import saludar, listar_estudiantes, listar_profesores
 
 urlpatterns = [
     path("saludar/",saludar,),
-    path("lista-alumnos/", listar_estudiantes, name="listar_alumnos"),
+    path("lista-alumnos/", listar_estudiantes, name="listar_alumnos"), #El name= sirve para que si cambia la url puedo seguir teniendo el mismo nombre en el html
     path("lista-profesores/", listar_profesores, name="listar_profesores"),
 ]
